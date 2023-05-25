f650543939fcb0ae96d5be2d5b21d622adeff081
shershon <shershon.japs@gmail.com>
2023-05-25 16:58:47 +0530

    Reverting to old URI

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,14 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-// const db = new Sequelize('ctftest1', 'postgres', 'thisisnotgoodlol', {
-//     host: 'database-2.cvyau4xzirke.ap-south-1.rds.amazonaws.com',
-//     dialect: 'postgres'
-//   });
-
-const db = new Sequelize('postgres//postgres:thisisnotgoodlol@database-2.cvyau4xzirke.ap-south-1.rds.amazonaws.com/ctftest1', {
-        host: 'database-2.cvyau4xzirke.ap-south-1.rds.amazonaws.com',
-        dialect: 'postgres'
-      });
+const db = new Sequelize('ctftest1', 'postgres', 'thisisnotgoodlol', {
+    host: 'database-2.cvyau4xzirke.ap-south-1.rds.amazonaws.com',
+    dialect: 'postgres'
+  });
     
 
 db.authenticate()
