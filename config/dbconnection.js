335fd927c96cce70650249fd9b6890b41fff54cf
shershon <shershon.japs@gmail.com>
2023-05-25 16:24:35 +0530

    Messages

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,9 +1,22 @@
 const { Sequelize } = require("sequelize");
 
+// const db = new Sequelize('ctftest1', 'postgres', 'thisisnotgoodlol', {
+//     host: 'database-2.cvyau4xzirke.ap-south-1.rds.amazonaws.com',
+//     dialect: 'postgres'
+//   });
+
 const db = new Sequelize('ctftest1', 'postgres', 'thisisnotgoodlol', {
     host: 'database-2.cvyau4xzirke.ap-south-1.rds.amazonaws.com',
-    dialect: 'postgres'
-  });
+    port: 5432,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl:'Amazon RDS'
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    language: 'en'
+})
 
 db.authenticate()
 .then(()=>{ 
